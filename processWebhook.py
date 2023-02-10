9b171bbb0e89e5cadbf24bd73b7e43a39b456197
Gabriel Ruiz Burgos <50720951+Dop1ta@users.noreply.github.com>
2023-02-10 15:27:02 -0300

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -28,7 +28,7 @@ def whatsapp():
     message = request.form['Body']
     senderId = request.form['From'].split('+')[1]
 
-    openai.api_key = "sk-api_key"
+    openai.api_key = "api_key"
     
     response = openai.Completion.create(
         engine = "text-davinci-003",
